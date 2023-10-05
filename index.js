8122fe9ea39873148857cb74fc6e635b3c0767a1
Amardeep Singh <amardeep.developper@gmail.com>
2023-10-05 10:39:37 +0530

    added welcome route

--- index.js
+++ index.js
@@ -9,30 +9,40 @@ const port = 3000;
 app.use(express.json());
 
 app.get("/generate-pdf", async (req, res) => {
-  // reading the template file for rendering
-  const template = fs.readFileSync("./template.html", { encoding: "utf8" });
-  const filledTemplate = Mustache.render(template, {});
+  res.send("Welcome to pdf generator");
+});
 
-  //   puppeteer will launch headless browser and use the template tp set content of page
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: [`--window-size=1920,1080`],
-    defaultViewport: {
-      width: 940,
-      height: 1080,
-    },
-  });
+app.get("/generate-pdf", async (req, res) => {
+  try {
+    // reading the template file for rendering
+    const template = fs.readFileSync("./template.html", { encoding: "utf8" });
+    const filledTemplate = Mustache.render(template, {});
 
-  const page = await browser.newPage();
-  await page.setContent(filledTemplate);
+    //   puppeteer will launch headless browser and use the template tp set content of page
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: [`--window-size=1920,1080`],
+      defaultViewport: {
+        width: 940,
+        height: 1080,
+      },
+    });
 
-  const pdfBuffer = await page.pdf({ printBackground: true });
+    const page = await browser.newPage();
+    await page.setContent(filledTemplate);
 
-  await browser.close();
-  res.setHeader("Content-Type", "application/pdf");
-  // Disbale if default download is not required
-  res.setHeader("Content-Disposition", "attachment; filename=generated.pdf");
-  res.send(pdfBuffer);
+    const pdfBuffer = await page.pdf({ printBackground: true });
+
+    await browser.close();
+    res.setHeader("Content-Type", "application/pdf");
+    // Disbale if default download is not required
+    res.setHeader("Content-Disposition", "attachment; filename=generated.pdf");
+    res.send(pdfBuffer);
+  } catch (error) {
+    res
+      .statusCode(500)
+      .send({ message: error?.message || "SOmething went wrong" });
+  }
 });
 
 app.listen(port, () => {
